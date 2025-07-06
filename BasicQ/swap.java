082543cda860a9bf155263e1399a9e8d19b3a2fc
Patel purv <patelpurv2643@gmail.com>
2025-07-06 11:00:13 +0530

    Add files via upload

---
+++ BasicQ/swap.java
@@ -0,0 +1,11 @@
+public class swap {
+    public static void main(String[] args){
+        int a=2;
+        int b=3;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("a after swaping : "+ a);
+        System.out.println("b after swaping : "+b);
+    }
+}
