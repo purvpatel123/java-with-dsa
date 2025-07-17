3e38b707536ee109074fe23bf0295cd9043f26cb
Patel purv <patelpurv2643@gmail.com>
2025-07-17 21:16:46 +0530

    Update Fibonaci.java

--- BasicQ/Fibonaci.java
+++ BasicQ/Fibonaci.java
@@ -1,23 +1,19 @@
-public class Fibonaci {
-     public static void fibonaci(int n){
-         int first=0;
-         int second=1;
-         int next;
+public class Fibonacci {
+    public static void fibonaci(int n) {
+        int first = 0;
+        int second = 1;
 
-         System.out.print("Fibonacci series up to " + n + ": ");
+        System.out.print("First " + n + " Fibonacci numbers: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
 
-         while (second<=n){
-             System.out.print(first + " ");
-             next=first+second;
-             first=second;
-             second=next;
-         }
-     }
-
-
-
-    public static void main(String[] args){
-        int n=5;
-       fibonaci(n);
+    public static void main(String[] args) {
+        int n = 10;
+        fibonaci(n);
     }
 }
